57a802bba1773f26da45cb35c8cbd19511178649
nitultalukdar <nitul.talukdar622@gmail.com>
2021-08-19 13:54:45 +0530

    revert back

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-module "vpcmodule" {
-	source = "./modules/vpc_module"
-	cidr_block = "${var.cidr}"
-	}
+provider "aws" {
+    profile = "default"
+    region     = "${var.region}"
+}
+
+
+
+
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "${var.cidr}"
+  tags = {
+     Name = "demo_vpc"
+}
+}
